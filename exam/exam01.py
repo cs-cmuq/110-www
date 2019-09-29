a533e0bb5be2727ac38607ac4494de7f6a57dce1
Gianni Di Caro <gdicaro@cmu.edu>
2019-09-29 15:03:35 +0300

    exam

---
+++ exam/exam01.py
@@ -0,0 +1,18 @@
+
+def clean_and_join(L, trash):
+    return
+
+
+
+def list_sort(L, s):
+    return
+
+
+
+def nested_lists(l, i, j, k):
+    if len(l) >= 
+        if len(l[i]) 
+            if type(l[i][j]) =
+                if len(l[i][j]
+                    return 
+    return
