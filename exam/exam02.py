c6edb03cb910f02cab4e05c8f09118954dd8a6a5
Gianni Di Caro <gdicaro@cmu.edu>
2019-10-27 13:19:44 +0300

    exam02

---
+++ exam/exam02.py
@@ -0,0 +1,34 @@
+
+
+def no_repeat(l):
+    return 
+
+
+def floor_matrix(m):
+    return
+
+#m = [ [1.8, 2,-5.3], [9.7, 0.5, 2.3], [1.2, 8.1, -2] ]
+
+
+def get_digit(num, pos):
+    return
+    
+
+def process(s):
+    return 
+
+# s = '''rick  , 10, 10 , 10 , 9.7, 8.7
+#        morty , 8 , 7.5, 10 , 9  , 7.6
+#        beth  , 7 , 9.6, 8.5, 10
+#        jerry , 6 , 5.4, 3.8, 10
+#        summer, 10, 9.5, 8.5, 5  , 7.2, 8''' 
+
+#d = {'beth': [7.0, 9.6, 8.5, 10.0],
+#     'jerry': [6.0, 5.4, 3.8, 10.0],
+#     'morty': [8.0, 7.5, 10.0, 9.0, 7.6],
+#     'rick': [10.0, 10.0, 10.0, 9.7, 8.7],
+#     'summer': [10.0, 9.5, 8.5, 5.0, 7.2, 8.0]
+#     }
+
+def compute_avg(d):
+    return
