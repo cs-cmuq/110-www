d61718664fd50ec94f39ac23d10fc4c28c9ee1eb
Gianni Di Caro <gdicaro@cmu.edu>
2019-12-05 15:11:01 +0300

    hwexam02

---
+++ hwexam/hwexam02.py
@@ -0,0 +1,42 @@
+def filter_text(text):
+    return
+
+def most_frequent_ignore_case(words):
+    return
+
+def most_frequent_form(words, word):
+    return
+
+def most_common_word(text):
+    return
+
+
+def simple_subseq_match(sequence, subseq):
+    return
+
+def subseq_match(sequence, subseq):
+    return
+
+def plot_points(x, y, line=True):
+  plt.figure()
+  plt.xlabel('x')
+  plt.ylabel('y')
+  if line:
+      linestyle = '-'
+  else:
+      linestyle = ''
+  plt.plot(x, y, marker='.', linestyle=linestyle)
+
+    
+def plot_function(f, xmin, xmax, step, lines=True):
+    return
+
+  
+def parabola(x):
+    return 
+
+def cubic(x):
+    return 
+
+def periodic(x):
+    return 
